7ab2d9ad539e8e08b3977186e3e72b498cf879f3
siva-mani-r3 <sivamaniramayanam7036@gmail.com>
2025-09-06 23:27:37 +0530

    up

--- src/redux/slices/verifyDocument.js
+++ src/redux/slices/verifyDocument.js
@@ -1,35 +1,52 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
-import {getCertificateDetails} from '../../utils/proofMintApi';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getCertificateDetails } from "../../utils/proofMintApi";
 
 export const getCerificate = createAsyncThunk(
-  'getCertificate',
-  async (documentId) => {
-    const certificateDetails = await getCertificateDetails(documentId);
-    return certificateDetails; 
+  "verifyDocument/getCerificate",
+  async (documentId, { rejectWithValue }) => {
+    try {
+      const certificateDetails = await getCertificateDetails(documentId);
+      if (!certificateDetails) {
+        return rejectWithValue("Document not found");
+      }
+      return certificateDetails;
+    } catch (err) {
+      return rejectWithValue("Document not found");
+    }
   }
 );
 
-
 const initialState = {
-  documentId: '',
+  documentId: "",
   verified: false,
   certificate: null,
+  error: null,
 };
+
 const verifyDocument = createSlice({
-  name: 'verifyDocument',
+  name: "verifyDocument",
   initialState,
   reducers: {
     setDocumentId: (state, action) => {
       state.documentId = action.payload;
     },
+    clearCertificate: (state) => {
+      state.certificate = null;
+      state.error = null;
+    },
   },
  extraReducers: (builder) => {
-  builder.addCase(getCerificate.fulfilled, (state, action) => {
-    state.certificate = action.payload;
-  });
+  builder
+    .addCase(getCerificate.fulfilled, (state, action) => {
+      state.certificate = action.payload;
+      state.error = null;
+    })
+    .addCase(getCerificate.rejected, (state, action) => {
+      state.certificate = null;
+      state.error = action.payload || "Document not found";
+    });
 }
-
 });
 
-export const {setDocumentId} = verifyDocument.actions;
+export const { setDocumentId, clearCertificate } = verifyDocument.actions;
 export default verifyDocument.reducer;
