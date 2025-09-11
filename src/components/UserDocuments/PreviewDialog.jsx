ce375426243e5c24b44fa90815232b05ddc444fb
Aayush Ranjan <aayushranjan@Aayushs-MacBook-Air.local>
2025-09-11 09:44:46 +0530

    changes

--- src/components/UserDocuments/PreviewDialog.jsx
+++ src/components/UserDocuments/PreviewDialog.jsx
@@ -15,7 +15,9 @@ const PreviewDialog = ({isOpen, docUrl, docTitle, onClose, onDownload}) => {
         </Button>,
       ]}
       width="80vw"
-      bodyStyle={{height: '70vh', display: 'flex', flexDirection: 'column'}}
+      styles={{
+        body: {height: '70vh', display: 'flex', flexDirection: 'column'},
+      }}
       centered
     >
       <div className="flex-1 overflow-auto flex items-center justify-center">
